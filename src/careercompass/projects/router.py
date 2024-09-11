6ab509cd5c830463bb4ad8dac5836942d5930536
Michael Hall <michael@rubberduck-labs.com>
2024-09-11 03:30:10 +0000

    Adding create and view projects

--- src/careercompass/projects/router.py
+++ src/careercompass/projects/router.py
@@ -28,3 +28,29 @@ def list_user_projects(user: User = Depends(check_current_user_active), db: Sess
     return project_list
 
 
+@project_router.get("/{project_id}", response_model=ProjectSchema)
+def get_project(user: User = Depends(check_current_user_active), db: Session = Depends(get_db)):
+    user_project = get_user_project(
+        db = db, 
+        project_id = project_id
+    )
+    if user_project.user_id == user.id: 
+        return user_project
+    else: 
+        raise  HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not authorized to view this project"
+        )
+
+
+@project_router.post("/create", response_model=ProjectSchema)
+def new_project(new_project: CreateProjectSchema, user: User = Depends(check_current_user_active), db: Session = Depends(get_db)):
+    logger.info("Creating new project")
+    logger.debug(f"New Project: {new_project}")
+    project_response = create_user_project(
+        db = db, 
+        user_id = user.id, 
+        new_project = new_project
+    )
+
+    return project_response
